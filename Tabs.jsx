a7f09e86b48b8393b907c9c8f5f6b15a6723e466
Cel200602 <5celine9c@gmail.com>
2026-02-05 10:29:55 +0530

    Add files via upload

---
+++ Tabs.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+function Tabs(){
+    const[activeTab,setActiveTab]=useState("Home")
+    return(
+        <div style={Styles.Container}>
+            <h2>Tab Component</h2>
+            <div style={Styles.tabContainer}>
+                <button style={activeTab==="Home" ? Styles.activeBtn: Styles.btn} 
+                onClick={()=>setActiveTab("Home")}>Home</button>
+
+                <button style={activeTab==="About" ? Styles.activeBtn: Styles.btn}
+                onClick={()=>setActiveTab("About")}>About</button>
+
+                <button style={activeTab==="Content" ? Styles.activeBtn: Styles.btn}
+                onClick={()=>setActiveTab("Content")}>Content</button>
+
+            </div>
+            <div style={Styles.contentBox}>
+                {activeTab==="Home"&&(
+                    <p>This is Home page</p>
+                )}
+                 {activeTab==="About"&&(
+                    <p>This is the About page of React JS</p>
+                )}
+                {activeTab==="Content"&&(
+                    <p>This page is about the overall content of React JS</p>
+                )}
+            </div>
+
+        </div>
+    )
+}
+const Styles={
+    Container:{
+        minHeight:"100vh",
+        background:"lightpink",
+        textAlign:"center"
+    }, 
+    tabContainer:{
+        padding:"20px"
+    },
+    contentBox:{
+        height:"100px",
+        width:"300px",
+        border:"1px solid",
+        borderRadius:"10px",
+        marginLeft:"470px"
+
+    },
+    activeBtn:{
+        background:"yellow"
+    },
+    btn:{
+        background:"white"
+    }}
+export default Tabs;
